a452e35760b1b3c096615f4ac890a9995245c33b
bastien612 <bastien.meunier@yahoo.fr>
2019-04-22 16:05:02 +0200

    add initial project

---
+++ src/com/juliette/sudoku/Main.java
@@ -0,0 +1,10 @@
+package com.juliette.sudoku;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Grid myGrid = new Grid(1,2,3,4?5?6?6);
+		// myGrid.resolve();
+		// println(myGrid)
+	}
+}
---
+++ src/com/juliette/sudoku/grid/Grid.java
@@ -0,0 +1,15 @@
+package com.juliette.sudoku.grid;
+
+import java.util.ArrayList;
+
+public class Grid {
+
+	 ArrayList<SubGrid> rowOne;
+	 ArrayList<SubGrid> rowTwo;
+	 ArrayList<SubGrid> rowThree;
+	 
+	 void resolve() {
+		 
+	 }
+
+}
